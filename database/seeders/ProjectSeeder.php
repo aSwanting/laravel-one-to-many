1098e1c3b414ff4079fa11ac05003fce393f3def
aSwanting <gab.damico@gmail.com>
2024-01-09 21:27:21 +0100

    add: projects faker seeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -5,13 +5,23 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use Faker\Generator as Faker;
+use App\Models\Project;
+use Illuminate\Support\Str;
+
 class ProjectSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i = 0; $i < 100; $i++) {
+            $new_project = new Project();
+            $new_project->title = $faker->sentence(5);
+            $new_project->slug = Str::slug($new_project->title, '-');
+            $new_project->description = $faker->text(500);
+            $new_project->save();
+        }
     }
 }
